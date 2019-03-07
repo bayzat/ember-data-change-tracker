ec2d6c16ba0fa4fb5e8274ed075aa23807fce4ce
Jiří Prokop <jprokop@synaptiko.cz>
2019-03-07 12:54:41 +0100

    Fix usage together with ember-cli-mirage, initializer has to be run before mirage is initialized

--- app/initializers/ember-data-change-tracker.js
+++ app/initializers/ember-data-change-tracker.js
@@ -3,5 +3,6 @@ import {initializer as initialize} from 'ember-data-change-tracker';
 export default {
   name: 'ember-data-change-tracker',
   after: 'ember-data',
+  before: 'ember-cli-mirage',
   initialize
 };
